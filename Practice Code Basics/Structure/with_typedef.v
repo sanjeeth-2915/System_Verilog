ed02edbd66aaab118dc141ca341f2a6a4cfb9b98
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-06 22:20:41 +0530

    Create with_typedef.v

---
+++ Practice Code Basics/Structure/with_typedef.v
@@ -0,0 +1,17 @@
+module struct_example;
+typedef struct {
+string name;
+int roll_num;
+int mark;
+} student_detail;
+student_detail st1,st2;
+initial begin
+st1.name = "Kathir";
+st1.roll_num = 3;
+st1.mark = 484;
+st2 ='{"Velan", 7, 350 };
+$display("Student detail st1: %p", st1);
+$display("Student detail st2: %p", st2);
+$display("Student: Name = %s, Roll number = %0dMark=%0d", st1.name, st1.roll_num, st1.mark);
+end
+endmodule
