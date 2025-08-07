c9ab09becf291ad327582708b0fc9545a0eab9f0
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-07 11:55:43 +0530

    Update Static_array_ex1.v

--- Practice Code Basics/Static Array/Static_array_ex1.v
+++ Practice Code Basics/Static Array/Static_array_ex1.v
@@ -14,3 +14,19 @@ $display("def_b[%0d] = %0d", i, def_b[i]);
 end
 end
 endmodule
+
+
+/*Result
+Default value of a[3] = 0
+Default value of a[2] = 0
+Default value of a[1] = 0
+Default value of a[0] = 0
+def_a[0] = 8
+def_a[1] = 8
+def a[2] = 8
+def_a[3] = 8
+def_b[3] = 1
+def_b[2] = 1
+def b[1] = 1
+def_b[0] = 1
+*/
