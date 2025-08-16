0d45279ff04845423a4c0c233b6043bac79d199f
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-16 22:11:16 +0530

    Update static_class_ex3.v

--- Practice Code Basics/class/static class/static_class_ex3.v
+++ Practice Code Basics/class/static class/static_class_ex3.v
@@ -1,1 +1,36 @@
+class sample;
+static int s_count;
+static function count_fun();
+s_count++;
+endfunction
+endclass
 
+module class_example;
+sample s1[7];
+initial begin
+  foreach (s1[i]) begin
+    s1[i] = new();
+    s1[i].count_fun();
+    Sdisplay("Value of s_count[%d]= %0d", i,s1[i].s_count);
+  end
+end
+endmodule
+
+class sample;
+int s_count;
+static function count_fun();
+  s_count++;
+  endfunction
+endclass
+
+module class_example;
+sample s1[7];
+  
+initial begin
+    foreach (s1[i]) begin
+    s1[i] = new();
+    s1[i].count_fun();
+    Sdisplay("Value of s_count[%d]= %0d", i,s1[i].s_count);
+    end
+end
+endmodule
