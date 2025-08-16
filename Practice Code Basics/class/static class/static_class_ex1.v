10e3f385856526002e5f41029e3aa2f8184a1b13
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-16 22:08:14 +0530

    Update static_class_ex1.v

--- Practice Code Basics/class/static class/static_class_ex1.v
+++ Practice Code Basics/class/static class/static_class_ex1.v
@@ -21,3 +21,22 @@ Sdisplay("Value of s_count[%d] - %0d", i, s1[i].s_count);
 end
 end
 endmodule
+
+
+/*Result
+
+#Value of s_count [0] = 1, count [0] = 1
+#Value of s_count [1] = 2, count [1] = 1
+#Value of s_count [2] = 3, count [2] = 1
+#Value of s_count [3] = 4, count [3] = 1
+#Value of s_count [4] = 5, count [4] = 1
+#Value of s_count [5] = 6, count [5] = 1
+#Value of s_count [6] = 7, count [6] = 1
+#Value of s_count [0] = 7
+#Value of s_count [1] = 7
+#Value of s_count [2] = 7
+#Value of s_count [3] = 7
+#Value of s_count [4] = 7
+#Value of s_count [5] = 7
+#Value of s_count [6] = 7
+*/
