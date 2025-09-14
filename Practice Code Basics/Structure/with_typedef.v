6edf5c6b90dc04b065345bb1c471fc79281fe757
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-09-14 22:56:09 +0530

    Update with_typedef.v

--- Practice Code Basics/Structure/with_typedef.v
+++ Practice Code Basics/Structure/with_typedef.v
@@ -12,6 +12,6 @@ st1.mark = 484;
 st2 ='{"Velan", 7, 350 };
 $display("Student detail st1: %p", st1);
 $display("Student detail st2: %p", st2);
-$display("Student: Name = %s, Roll number = %0dMark=%0d", st1.name, st1.roll_num, st1.mark);
+  $display("Student: Name = %s, Roll number = %0d Mark=%0d", st1.name, st1.roll_num, st1.mark);
 end
 endmodule
